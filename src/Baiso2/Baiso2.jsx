15d6bca0c1560987a62eb101088271100180dcb3
leepsyko <letho161299@gmail.com>
2023-08-24 09:55:29 +0700

    xoá header

--- src/Baiso2/Baiso2.jsx
+++ src/Baiso2/Baiso2.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import Body from './Body/Body'
-import Header from './Header.jsx/Header'
 
 export default function Baiso2() {
   return (
     <div className='h-100' style={{backgroundImage: "url(./glassesImage/background.jpg)",backgroundSize:"Cover"}}>
-      <Header />
       <Body />
     </div>
   )
